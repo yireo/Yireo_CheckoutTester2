67485b0bde2a3252a771710d067540a040b73ba9
Jisse Reitsma <jisse@yireo.com>
2017-06-05 19:47:06 +0200

    Pending changes

---
+++ Block/Success.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Yireo CheckoutTester2 for Magento
+ *
+ * @package     Yireo_CheckoutTester2
+ * @author      Yireo (https://www.yireo.com/)
+ * @copyright   Copyright 2016 Yireo (https://www.yireo.com/)
+ * @license     Open Source License (OSL v3)
+ */
+
+namespace Yireo\CheckoutTester2\Block;
+use \Magento\Framework\View\Element\Template;
+
+class Success extends Template
+{
+}
