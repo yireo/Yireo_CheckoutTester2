3e7a0a2d3c8d5d960e0c37fe968c8c8e5876783c
Jisse Reitsma <info@yireo.com>
2017-08-09 07:59:20 +0200

    Add native exceptions

---
+++ Exception/InvalidOrderId.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * CheckoutTester2 plugin for Magento
+ *
+ * @package     Yireo_EmailTester2
+ * @author      Yireo (https://www.yireo.com/)
+ * @copyright   Copyright 2017 Yireo (https://www.yireo.com/)
+ * @license     Open Source License (OSL v3)
+ */
+
+namespace Yireo\CheckoutTester2\Exception;
+
+/**
+ * Class InvalidOrderId
+ *
+ * @package Yireo\CheckoutTester2\Exception
+ */
+class InvalidOrderId extends \Exception
+{
+
+}
